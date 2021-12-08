9bc35c32f5f3191a3b4006090497d896b04a0bcc
888Sourav888 <77108272+888Sourav888@users.noreply.github.com>
2021-12-08 21:47:19 +0530

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello");
+            string row = Console.ReadLine();
+            string column = Console.ReadLine();
+            // convert to integer
+            int m = Convert.ToInt32(row);
+            int n = Convert.ToInt32(column);
+            int[,] input = new int[m, n];
+            
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.WriteLine("substring");
+                    input[i,j] = Convert.ToInt32(Console.ReadLine());
+                }
+                //Console.WriteLine("Hello, World!"+i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
