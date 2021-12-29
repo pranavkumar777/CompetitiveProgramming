2b02ec6fa622031b8c0fb333a8980f7b77c01bbf
888Sourav888 <77108272+888Sourav888@users.noreply.github.com>
2021-12-29 09:31:35 +0530

    Renamed and added testing

---
+++ Testing/GameOfLifeTesting.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using GameOfLife;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testing
+{
+    [TestClass]
+    public class GameOfLifeTesting
+    {
+        [TestMethod]
+        public void GameofLifeTestCase()
+        {
+            int[,] currGen = new int[25, 25];
+            bool success = true;
+            //Input set 1
+            int gen = 3;
+            currGen[2, 1] = 1;
+            currGen[2, 2] = 1;
+            currGen[2, 3] = 1;
+            List<int> TestLivePositions = new List<int>() {
+            12,22,32
+            };
+            GameOfLifeSolution gameOfLifeSolution = new GameOfLifeSolution();
+            List<int> LivePositions = gameOfLifeSolution.ComputeLivingPositions(currGen, gen);
+            for (int i = 0; i < LivePositions.Count; i++)
+            {
+                if (!TestLivePositions.Contains(LivePositions[i]))
+                {
+                    success = false;
+                }
+            }
+            Assert.IsTrue(success);
+        }
+    }
+}
---
+++ Testing/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Testing")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Testing")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d31568ff-a1d2-4868-9b45-2a0e6936992f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
